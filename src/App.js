51662c16e6b5e12fbe69777d56b010b494e7edf5
IsaacMartin <79712755+IsaacGilgames@users.noreply.github.com>
2025-05-29 21:21:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import HomePage from "./homepage"
-
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./homepage"
+import Versenyzok from "./Versenyzok"
+import "./bootstrap.min.css"
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+   <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/resztvevok" element={<Versenyzok />}></Route>
+        </Routes>
+      </BrowserRouter>
+   </div>
   );
 }
 
