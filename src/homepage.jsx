3ccd16ba23ac82920f6a559b3b682565a3ee050e
IsaacMartin <79712755+IsaacGilgames@users.noreply.github.com>
2025-05-29 20:53:46 +0200

    Update homepage.jsx

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -7,46 +7,51 @@ export default function Homepage() {
     
 
   return (
-    <div class="OpenPage">
-        <div class="container-fluid">
-
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link" href="#">Diákláb Liga</a>
+    <div className="OpenPage">
+        <div className="container-fluid">
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul className="navbar-nav">
+            <li className="nav-item">
+            <Link className="nav-link" to="/">Diákláb Liga</Link>
             </li >
-            <li class="nav-item">
-            <a class="nav-link" href="#">Versenyzők</a>
+            <li className="nav-item">
+            <Link className="nav-link" to="/resztvevok">Versenyzők</Link>
             </li>
         </ul>
         </nav>
 
-
-      <div class="jumbotron jumbotron-fluid text-white text-center d-flex align-items-center justify-content-center fade-in" >
-        <div class="bg-dark p-4 rounded-lg">
-          <h1 class="display-4">Lépj pályára!</h1>
-          <p class="lead">Vállald a kihívást, vegyél részt versenyünkön!</p>
-          <a class="btn btn-success btn-lg" href="/resztvevok">Regisztrált csapatok!</a>
+      {/* Hero Section */}
+      <div className="jumbotron jumbotron-fluid text-white text-center d-flex align-items-center justify-content-center fade-in" >
+        <div className="bg-dark p-4 rounded-lg">
+          <h1 className="display-4">Lépj pályára!</h1>
+          <p className="lead">Vállald a kihívást, vegyél részt versenyünkön!</p>
+          <Link className="btn btn-success btn-lg" to="/resztvevok">Regisztrált csapatok!</Link>
         </div>
       </div>
       
       </div>
-      <div class='urlap'>
+      <div className='urlap'>
 
         <h1>Regisztráció</h1>
-        <div class="container vertical center">
-        <div class="title">Nevezési LAP</div>
-        <div class="content vertical center">
-            <div class="field horizontal center">
-                <label class="label">Csapatnév:</label>
-                <input type="text" id="persons">
+        
+
+        <div className="container vertical center">
+        <div className="title">Nevezési LAP</div>
+        <div className="content vertical center">
+            <div className="field horizontal center">
+                <label className="label">Csapatnév:</label>
+                <input type="text" id="persons"/>
                 
             </div>
             
 
-            <div class="field horizontal center" >
+            <div className="field horizontal center" >
             <label htmlFor="category">Versenykategória:</label>
-                <select id="category" class="form-control">
+                <select
+                id="category"
+                className="form-control"
+                value={category}>
               <option></option>
               <option value="alsós">alsós</option>
               <option value="felsős">felsős</option>
@@ -54,22 +59,22 @@ export default function Homepage() {
             </select>
             </div>
            
-            <div class="field horizontal center">
-                <label class="label">Győzelem</label>
-                <input type="number" id="persons" >
+            <div className="field horizontal center">
+                <label className="label">Győzelem</label>
+                <input type="number" id="persons"  min="1" />
             </div>
-            <div class="field horizontal center" >
-                <label class="label">Kép:</label>
-                <input id="url" type="url" >
+            <div className="field horizontal center" >
+                <label className="label">Kép:</label>
+                <input id="url" type="url"  />
             </div>
             <label class="foci-checkbox">
                 <input id="meccs-checkbox" type="checkbox" name="meccs" value="igen"/>
                 Felhasználói feltételek elfogadása
                 <span class="checkmark"></span>
                 </label>
-            <div class="horizontal center">
-                <div class="label"></div>
-                <button id="saveButton" title="Mentés">MENTÉS</button>
+            <div className="horizontal center">
+                <div className="label"></div>
+                <button id="saveButton" title="Mentés" >MENTÉS</button>
             </div>
         </div>
     </div>
